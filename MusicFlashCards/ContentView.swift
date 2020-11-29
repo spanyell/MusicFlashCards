4dabf3eed68a4b704ef3c1073df86fda894f8303
Dan Beers <spanyell@icloud.com>
2020-11-28 18:10:26 -0800

    Tested an image.

--- MusicFlashCards/ContentView.swift
+++ MusicFlashCards/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("This is a middle C.")
+        Image("trebleC4")
             .padding()
     }
 }
