3b70512cc472d494873a0bc9203c14a89d5715ac
Dan Beers <spanyell@icloud.com>
2020-11-28 16:00:09 -0800

    Initial Commit

---
+++ MusicFlashCards/MusicFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFlashCardsApp.swift
+//  MusicFlashCards
+//
+//  Created by Dan Beers on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
