ffb0d70844093e47a0b1e4f8beeb82bd190b3729
Dan Beers <spanyell@icloud.com>
2020-12-14 12:19:33 -0800

    Made some mock data for the flashcard view.

--- MusicFlashCards/ContentView.swift
+++ MusicFlashCards/ContentView.swift
@@ -16,48 +16,8 @@ struct ContentView: View {
         VStack {
             Text("Music Flash Cards")
                 .font(.headline)
-            ZStack {
-                VStack {
-                    Text("Question1")
-                    Text("What is this note?")
-                    Image("trebleC4")
-                        
-                    ZStack {
-                        ButtonAnswer()
-                        Text("F")
-                    }
-                    ZStack {
-                        ButtonAnswer()
-                        Text("E flat")
-                    }
-                    ZStack {
-                        ButtonAnswer()
-                        Text("G")
-                    }
-                    ZStack {
-                        ButtonAnswer()
-                            .onTapGesture {
-                                correctAnswer.toggle()
-                            }
-                        Text("Middle C")
-                    }
-                    ZStack {
-                        ButtonAnswer()
-                            .onTapGesture {
-                                incorrectAnswer.toggle()
-                            }
-                        Text("A sharp")
-                    }
-                }
-                CorrectAnswer()
-                    .offset(y: correctAnswer ? 0 : 600)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-                
-                IncorrectAnswer()
-                    .offset(y: incorrectAnswer ? 0 : -600)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-                
-            }
+            ForEach(sectionData) { item in
+                FlashCard(section: item)
         }
     }
 }
@@ -81,7 +41,7 @@ struct ButtonAnswer: View {
 struct CorrectAnswer: View {
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerRadius: 25)
                 .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))]), startPoint: .top, endPoint: .bottom))
                 .frame(width: 250, height: 300)
                 }
@@ -100,4 +60,65 @@ struct IncorrectAnswer: View {
         }
         
     }
+}
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var question: String
+    var image: Image
+    var answer1: String
+    var answer2: String
+    var answer3: String
+    var answer4: String
+    var answer5: String
+    var correctAnswer: String
+}
+
+let sectionData = [
+    Section(title: "Question 1", question: "What is this note?", image: Image("trebleC4"), answer1: "F", answer2: "E flat", answer3: "G", answer4: "Middle C", answer5: "A sharp", correctAnswer: "Middle C")
+]
+
+struct FlashCard: View {
+    var section: Section
+    var body: some View {
+        ZStack {
+            VStack {
+                Text(section.title)
+                Text(section.question)
+                section.image
+                
+                
+                ZStack {
+                    ButtonAnswer()
+                    Text(section.answer1)
+                }
+                ZStack {
+                    ButtonAnswer()
+                    Text(section.answer2)
+                }
+                ZStack {
+                    ButtonAnswer()
+                    Text(section.answer3)
+                }
+                ZStack {
+                    ButtonAnswer()
+                    Text(section.answer4)
+                }
+                ZStack {
+                    ButtonAnswer()
+                    Text(section.answer5)
+                }
+            }
+//            CorrectAnswer()
+//                .offset(y: correctAnswer ? 0 : 600)
+//                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+//
+//            IncorrectAnswer()
+//                .offset(y: incorrectAnswer ? 0 : -600)
+//                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            
+        }
+    }
+}
 }
