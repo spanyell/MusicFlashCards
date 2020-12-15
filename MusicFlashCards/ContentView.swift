20c25f99cecd00760c5c8b2c517855c347c7ee22
Dan Beers <spanyell@icloud.com>
2020-12-14 16:18:42 -0800

    Created a functioning trivia question.

--- MusicFlashCards/ContentView.swift
+++ MusicFlashCards/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var correctAnswer = false
-    @State var incorrectAnswer = false
-    
-    
     var body: some View {
         VStack {
             Text("Music Flash Cards")
-                .font(.headline)
+                .font(.system(size: 28, weight: .bold))
+                Spacer ()
+                
+            
             ForEach(sectionData) { item in
                 FlashCard(section: item)
         }
@@ -72,52 +71,102 @@ struct Section: Identifiable {
     var answer3: String
     var answer4: String
     var answer5: String
-    var correctAnswer: String
+    var correctAnswer: Double
 }
 
 let sectionData = [
-    Section(title: "Question 1", question: "What is this note?", image: Image("trebleC4"), answer1: "F", answer2: "E flat", answer3: "G", answer4: "Middle C", answer5: "A sharp", correctAnswer: "Middle C")
+    Section(title: "Question 1", question: "What is this note?", image: Image("trebleC4"), answer1: "F", answer2: "E flat", answer3: "G", answer4: "Middle C", answer5: "A", correctAnswer: 3)
+//    Section(title: "Question 2", question: "What is this note?", image: Image("trebleC4"), answer1: "F", answer2: "G flat", answer3: "A", answer4: "Middle C", answer5: "B", correctAnswer: 3),
+//    Section(title: "Question 3", question: "What is this note?", image: Image("trebleD5"), answer1: "D", answer2: "F", answer3: "F sharp", answer4: "Middle C", answer5: "B", correctAnswer: 0)
 ]
 
 struct FlashCard: View {
+    @State var correctAnswer = false
+    @State var incorrectAnswer = false
     var section: Section
     var body: some View {
         ZStack {
             VStack {
                 Text(section.title)
+                
+                Spacer()
+                
                 Text(section.question)
                 section.image
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    .cornerRadius(5)
                 
+                Spacer()
                 
                 ZStack {
                     ButtonAnswer()
+                        .onTapGesture {
+                            if section.correctAnswer == 0 {
+                                correctAnswer = true
+                            } else {
+                                incorrectAnswer = true
+                            }
+                        }
                     Text(section.answer1)
                 }
                 ZStack {
                     ButtonAnswer()
+                        .onTapGesture {
+                            if section.correctAnswer == 1 {
+                                correctAnswer = true
+                            } else {
+                                incorrectAnswer = true
+                            }
+                        }
                     Text(section.answer2)
                 }
                 ZStack {
                     ButtonAnswer()
+                        .onTapGesture {
+                            if section.correctAnswer == 2 {
+                                correctAnswer = true
+                            } else {
+                                incorrectAnswer = true
+                            }
+                        }
                     Text(section.answer3)
                 }
                 ZStack {
                     ButtonAnswer()
+                        .onTapGesture {
+                            if section.correctAnswer == 3 {
+                                correctAnswer = true
+                            } else {
+                                incorrectAnswer = true
+                            }
+                        }
                     Text(section.answer4)
                 }
                 ZStack {
                     ButtonAnswer()
+                        .onTapGesture {
+                            if section.correctAnswer == 4 {
+                                correctAnswer = true
+                            } else {
+                                incorrectAnswer = true
+                            }
+                        }
                     Text(section.answer5)
                 }
             }
-//            CorrectAnswer()
-//                .offset(y: correctAnswer ? 0 : 600)
-//                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-//
-//            IncorrectAnswer()
-//                .offset(y: incorrectAnswer ? 0 : -600)
-//                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            CorrectAnswer()
+                .offset(y: correctAnswer ? 0 : -600)
+                .animation(.spring(response: 0.7, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    correctAnswer.toggle()
+                }
             
+            IncorrectAnswer()
+                .offset(y: incorrectAnswer ? 0 : 1000)
+                .animation(.spring(response: 0.7, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    incorrectAnswer.toggle()
+                }
         }
     }
 }
